903da54834a5db0ff05501c85234d8f03f33dbe6
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-08-08 19:47:04 -0500

    start adding streak check helpers

--- lib/gameboard.rb
+++ lib/gameboard.rb
@@ -2,10 +2,13 @@ require 'pry-byebug'
 require_relative 'tokens'
 
 class Gameboard
+  attr_reader :last_index_placed
+
   def initialize(rows = 6, columns = 7)
     @columns = columns.freeze
     @rows = rows.freeze
-    @board = Array.new(@columns * @rows) { Tokens::EMPTY}
+    @board = Array.new(@columns * @rows) { Tokens::EMPTY }
+    @last_index_placed = nil
   end
 
   def to_s
@@ -32,10 +35,63 @@ class Gameboard
     column_contents
   end
 
+  def row(row)
+    row_contents = []
+    indices = row_indices(column)
+
+    indices.each do |index|
+      row_contents << @board[index]
+    end
+
+    row_contents
+  end
+
   def drop(column, token)
     indices = column_indices(column).reverse
     first_open = indices.find { |index| @board[index] == Tokens::EMPTY }
+
     @board[first_open] = token
+    @last_index_placed = first_open
+  end
+
+  def full?
+    @board.all? { |token| token != Tokens::EMPTY } 
+  end
+
+  def streak_length(index)
+    return 0 if @board[index] == Tokens::EMPTY || !index_inbounds?(index)
+
+    directions = [[0, 1], 
+                  [1, 0], 
+                  [1, 1], 
+                  [1, -1]]
+
+
+    # check left and right
+    # check up and down
+    # check \ diagonal
+    # check / diagonal
+  end
+
+  def valid_coord_move?(x, y, start_index)
+    return false if x < 0 && column_indices(1).include?(start_index)
+    return false if x > 0 && column_indices(@columns).include?(start_index)
+    return false if y < 0 && row_indices(1).include?(start_index)
+    return false if y > 0 && row_indices(@rows).include?(start_index)
+
+    true
+  end
+
+  def coord_to_index(x, y)
+    (y * @columns) + x
+  end
+
+  def index_to_coord(index)
+    [index % @columns, index / @columns]
+  end
+
+  def index_after_move(move_coords, index)
+
   end
 
   private 
@@ -44,9 +100,49 @@ class Gameboard
     indices = []
 
     @rows.times do |row_index|
-      indices << column + (row_index * @columns) - 1
+      indices << (row_index * @columns) + column - 1
     end
 
     indices
   end
+
+  def row_indices(row)
+    indices = []
+
+    @columns.times do |col_index|
+      indices << ((row - 1) * @columns) + col_index
+    end
+
+    indices
+  end
+
+  def index_inbounds?(index)
+    max_index = @board.size - 1
+    index.between?(0, max_index)
+  end
+
+  def index_in_direction(x, y, start_index)
+    new_index = start_index + ()
+  end
+
+  def recursive_measure(movement, index, token)
+    # base case, not in m
+  end
+
+  # def edge_of_row?(index)
+  #   row_position = index % @columns
+  #   row_end_remainder = @columns - 1
+  #   row_start_remainder = 0
+
+  #   [row_start_remainder, row_end_remainder].include? row_position
+  # end
+
+  # def edge_of_column?(index)
+  #   row_position = index % @columns
+  #   row_end_remainder = @columns - 1
+  #   row_start_remainder = 0
+
+  #   [row_start_remainder, row_end_remainder].include? row_position
+  # end
 end
+
--- spec/gameboard_spec.rb
+++ spec/gameboard_spec.rb
@@ -36,10 +36,122 @@ describe Gameboard do
         last_two = column[-2..-1]
         expect(last_two.all?(token)).to eq(true)
       end
-
     end
   end
 
-  describe '#empty' do
+  describe '#full?' do
+    context 'when 1 spot filled' do
+      subject(:unfilled_game) { described_class.new(2, 1) }
+
+      it 'is not full' do
+        unfilled_game.drop(1, 'X')
+
+        expect(unfilled_game).to_not be_full
+      end
+    end
+
+    context 'when all spots filled' do
+      subject(:filled_game) { described_class.new(2, 1) }
+
+      it 'is full' do
+        filled_game.drop(1, 'X')
+        filled_game.drop(1, 'X')
+
+        expect(filled_game).to be_full
+      end
+    end
+  end
+
+  describe '#last_index_placed' do
+    subject(:last_placed_game) { described_class.new(2, 2) }
+
+    context 'when dropped in col 1 of 2x2' do
+      it 'returns index 2 on first drop' do
+        last_placed_game.drop(1, 'X')
+        last_index = last_placed_game.last_index_placed
+
+        expect(last_index).to eq(2)
+      end
+
+      it 'returns index 0 on second drop' do
+        last_placed_game.drop(1, 'X')
+        last_placed_game.drop(1, 'X')
+        last_index = last_placed_game.last_index_placed
+
+        expect(last_index).to eq(0)
+      end
+    end
+  end
+
+  describe '#streak_length' do
+    context 'when index has no token' do
+      subject(:none_in_row) { described_class.new(2, 2) }
+
+      context 'when checking empty index' do
+        it 'returns 0' do
+          expect(none_in_row.streak_length(0)).to be_zero
+        end
+      end
+  
+      context 'when checking out of bounds index' do
+        it 'returns 0' do
+          expect(none_in_row.streak_length(100)).to be_zero
+        end
+      end
+    end
+  end
+
+  describe '#valid_coord_move?' do
+    subject(:coord_test_game) { described_class.new }
+
+    context 'when moving inside the board' do
+
+      it 'is a valid move' do
+        valid = coord_test_game.valid_coord_move?(1, 1, 0)
+        expect(valid).to eq(true)
+      end
+    end
+
+    context 'when movement is out of bounds' do
+      context 'when left boundary broken' do
+        it 'is not a valid move' do
+          x_move = -1
+          y_move = 1
+          left_column_index = 7
+          valid = coord_test_game.valid_coord_move?(x_move, y_move, left_column_index)
+          expect(valid).to eq(false)
+        end
+      end
+
+      context 'when right boundary broken' do
+        it 'is not a valid move' do
+          x_move = 1
+          y_move = 0
+          right_column_index = 27
+          valid = coord_test_game.valid_coord_move?(x_move, y_move, right_column_index)
+          expect(valid).to eq(false)
+        end
+      end
+
+      context 'when top boundary broken' do
+        it 'is not a valid move' do
+          x_move = 0
+          y_move = -1
+          top_row_index = 5
+          valid = coord_test_game.valid_coord_move?(x_move, y_move, top_row_index)
+          expect(valid).to eq(false)
+        end
+      end
+
+      context 'when bottom boundary broken' do
+        it 'is not a valid move' do
+          x_move = 0
+          y_move = 1
+          bottom_row_index = 40
+          valid = coord_test_game.valid_coord_move?(x_move, y_move, bottom_row_index)
+          expect(valid).to eq(false)
+        end
+      end
+    end
   end
 end
